cf47a08368c5d2e4eae6ed7e33c2550327761d2e
Gamaranto <gamarantor@gmail.com>
2020-04-03 16:27:27 +0200

    Add ProposalPreview and ProposalPreviewList

--- packages/joy-proposals/src/ProposalPreview/ProposalPreviewList.tsx
+++ packages/joy-proposals/src/ProposalPreview/ProposalPreviewList.tsx
@@ -2,13 +2,12 @@ import React from "react";
 
 import { Card, Tab } from "semantic-ui-react";
 import ProposalPreview, { ProposalPreviewProps } from "./ProposalPreview";
-import { ProposalPreviewList } from ".";
 
 type ProposalPreviewListProps = {
   proposals?: ProposalPreviewProps[];
 };
 
-function PreviewList({ proposals }: ProposalPreviewListProps) {
+export default function ProposalPreviewList({ proposals }: ProposalPreviewListProps) {
   return (
     <Card.Group className="ProposalPreviewList">
       {proposals.map((proposal, idx) => (
@@ -22,24 +21,3 @@ function PreviewList({ proposals }: ProposalPreviewListProps) {
     </Card.Group>
   );
 }
-
-const proposalsViews = [
-  {
-    menuItem: "Active"
-  },
-  {
-    menuItem: "Withdrawn"
-  },
-  {
-    menuItem: "Approved"
-  },
-  {
-    menuItem: "Slashed"
-  },
-  {
-    menuItem: "Rejected"
-  }
-];
-
-const ProposalPreviewList = () => <Tab panes={proposalsViews} />;
-export default ProposalPreviewList;
