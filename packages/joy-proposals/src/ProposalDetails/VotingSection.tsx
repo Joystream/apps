cfcdb6e75fd13c6844f020c60434c4b576054286
Gamaranto <gamarantor@gmail.com>
2020-04-06 17:08:11 +0200

    # This is a combination of 11 commits. # This is the 1st commit message:
    Merge pull request #355 from siman/alex-proposals-generic-form
    
    Proposals generic form, fix #326
    # This is the commit message #2:
    
    Markup Finished
    
    # This is the commit message #3:
    
    Add Stories
    
    # This is the commit message #4:
    
    Adjusted Styling on Details Page
    
    # This is the commit message #5:
    
    Add ProposalDetails.css And Clean Up Styles
    
    # This is the commit message #6:
    
    Add Colors To Votes Component
    
    # This is the commit message #7:
    
    Add Icons
    
    # This is the commit message #8:
    
    Update packages/joy-proposals/src/ProposalDetails/ProposalDetails.tsx
    
    Co-Authored-By: Alex Siman <aleksandr.siman@gmail.com>
    # This is the commit message #9:
    
    Update packages/joy-proposals/src/ProposalDetails/ProposalDetails.tsx
    
    Co-Authored-By: Alex Siman <aleksandr.siman@gmail.com>
    # This is the commit message #10:
    
    Update packages/joy-proposals/src/ProposalDetails/Votes.tsx
    
    Co-Authored-By: Alex Siman <aleksandr.siman@gmail.com>
    # This is the commit message #11:
    
    Apply suggestions from code review
    
    Co-Authored-By: Alex Siman <aleksandr.siman@gmail.com>

---
+++ packages/joy-proposals/src/ProposalDetails/VotingSection.tsx
@@ -0,0 +1,50 @@
+import React from "react";
+
+import { Icon, Button, Message, Divider, Header } from "semantic-ui-react";
+
+import { VoteValue } from "./ProposalDetails";
+import useVoteStyles from "./useVoteStyles";
+
+type VotingSectionProps = {
+  onVote?: (vote: VoteValue) => void;
+  hasVoted?: boolean;
+  value?: VoteValue;
+};
+
+export default function VotingSection({ onVote, hasVoted, value }: VotingSectionProps) {
+  if (hasVoted) {
+    const { icon, color } = useVoteStyles(value);
+
+    return (
+      <Message icon color={color}>
+        <Icon name={icon} />
+        <Message.Content>
+          You voted <span className="bold">{`"${value}"`}</span>
+        </Message.Content>
+      </Message>
+    );
+  }
+
+  return (
+    <>
+      <Header as="h3">Submit your vote</Header>
+      <Divider />
+      <Button color="green" icon labelPosition="left" onPress={() => onVote("Approve")}>
+        <Icon name="smile" inverted />
+        Approve
+      </Button>
+      <Button color="grey" icon labelPosition="left" onPress={() => onVote("Abstain")}>
+        <Icon name="meh" inverted />
+        Abstain
+      </Button>
+      <Button color="orange" icon labelPosition="left" onPress={() => onVote("Reject")}>
+        <Icon name="frown" inverted />
+        Reject
+      </Button>
+      <Button color="red" icon labelPosition="left" onPress={() => onVote("Slash")}>
+        <Icon name="times" inverted />
+        Slash
+      </Button>
+    </>
+  );
+}
